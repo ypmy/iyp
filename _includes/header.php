2c24f38e1253c3d521905b6f1ebc514e5a5922a0
Red4x <ryda.arush@gmail.com>
2014-06-12 11:01:39 +0800

    This is the Grid system code update

---
+++ _includes/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Yellowpages Malaysia</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<!-- CSS -->
+		<link href="assets/css/main.css" rel="stylesheet" />
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+	<style>
+	
+
+</style>
+
+	</head>
+
+
+<body>
+		
+<!-- Header -->
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="container">
+		<div id="header" class="row">
+			<div id="logo" class="col-md-3"></div>
+			<div id="nav" class="col-md-9">
+				<ul class="nav navbar-nav pull-right">
+        			<li class="active"><a href="#">Business</a></li>
+        			<li><a href="#">Personal</a></li>
+        			<li><a href="#">Going Out</a></li>
+        			<li><a href="#">Useful Hotlines</a></li>
+        			<li><a href="#"><span class="fa fa-lock"></span> Log In</a></li>
+        		</ul>
+			</div>
+		</div>
+	</div>	
+</div>
