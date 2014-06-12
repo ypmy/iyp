f32ab6b1fa8d35cc1ff49007258116eb3b026086
Red4x <ryda.arush@gmail.com>
2014-06-12 21:03:49 +0800

    HEADER Responsive - DONE

--- _includes/header.php
+++ _includes/header.php
@@ -7,30 +7,38 @@
 		<link href="assets/css/main.css" rel="stylesheet" />
 		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
 
-	<style>
-	
 
-</style>
-
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 	</head>
 
 
 <body>
-		
 <!-- Header -->
-<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-	<div class="container">
-		<div id="header" class="row">
-			<div id="logo" class="col-md-3"></div>
-			<div id="nav" class="col-md-9">
-				<ul class="nav navbar-nav pull-right">
-        			<li class="active"><a href="#">Business</a></li>
-        			<li><a href="#">Personal</a></li>
-        			<li><a href="#">Going Out</a></li>
-        			<li><a href="#">Useful Hotlines</a></li>
-        			<li><a href="#"><span class="fa fa-lock"></span> Log In</a></li>
-        		</ul>
-			</div>
-		</div>
-	</div>	
+
+
+<div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+  <div class="container">
+  		<a href ="#" id="logo" class="navbar-brand"></a>
+  	<div class="navbar-header navbar-right"> 
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		    <span class="sr-only">Toggle navigation</span>
+		    <span class="icon-bar"></span>
+		    <span class="icon-bar"></span>
+		    <span class="icon-bar"></span>
+		</button>
+	</div>
+	<div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="#">Business</a></li>
+        <li><a href="#">Personal</a></li>
+        <li><a href="#">Going Out</a></li>
+        <li><a href="#">Useful Hotlines</a></li>
+        <li><a href="#"><span class="fa fa-lock"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
 </div>
+ 
+
