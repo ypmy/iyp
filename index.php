c6924364854ed7f75164258c588d32742113fe29
Red4x <ryda.arush@gmail.com>
2014-06-13 14:48:39 +0800

    no message

---
+++ index.php
@@ -0,0 +1,40 @@
+<!-- Header -->
+<?php
+include('_includes/header.php');
+?>
+
+<!-- Body -->
+	<div class="nav-header">
+		<div class="container">
+			<div class="row ">				
+				<div id="switch-btn" class="col-md-3">SWITCH BUTTON</div>
+				<div id="search" class="col-md-9">SEARCH BUTTON </div>
+			</div>
+		</div>
+	</div>
+
+
+<div class="container">
+	<div id="content" class="row">
+		<div id="popular-categories" class="col-md-3 "><p>POPULAR CATEGORIES</p></div>
+		<div id="banner-slide" class="col-md-9"><p>BANNER SLIDESHOW</p></div>
+	</div>
+</div>
+
+<!-- Footer -->
+<?php
+include('_includes/footer.php');
+?>
+
+
+<!-- JS -->
+<script src="retina.js"></script>
+<script src="retina.min.js"></script>
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+    
+
+</body>
+
+	</html>
